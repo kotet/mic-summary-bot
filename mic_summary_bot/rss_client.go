358583df18cc17da833ac5ecf2029473ba848599
Kotet <kotet0418@gmail.com>
2025-06-14 11:19:29 +0900

    rss_client

---
+++ mic_summary_bot/rss_client.go
@@ -0,0 +1,38 @@
+package micsummarybot
+
+import (
+	"fmt"
+
+	"github.com/mmcdole/gofeed"
+)
+
+// RSSClient はRSSフィードの取得とパースを行うクライアント
+type RSSClient struct {
+	feedParser *gofeed.Parser
+}
+
+// NewRSSClient は新しいRSSClientインスタンスを作成します。
+func NewRSSClient() *RSSClient {
+	return &RSSClient{
+		feedParser: gofeed.NewParser(),
+	}
+}
+
+// FetchFeed は指定されたURLからRSSフィードを取得し、パースします。
+func (c *RSSClient) FetchFeed(url string) ([]*gofeed.Item, error) {
+	feed, err := c.feedParser.ParseURL(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse RSS feed from %s: %w", url, err)
+	}
+
+	var newItems []*gofeed.Item
+	for _, item := range feed.Items {
+		// published_at が存在しない場合はスキップ
+		if item.PublishedParsed == nil {
+			continue
+		}
+		newItems = append(newItems, item)
+	}
+
+	return newItems, nil
+}
